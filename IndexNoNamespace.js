3a450aa717c1be2548ba3ea043ac36855055839a
Warren Brown <warrengbrn@gmail.com>
2020-06-21 04:57:14 -0400

    added custom namespace

--- IndexNoNamespace.js
+++ IndexNoNamespace.js
@@ -7,35 +7,31 @@ app.get('/', (req, res) => {
   });
 
 //code executes when a connection is received from client
-
+io.on('connection', (socket) => {
     
-//custom namespace
-const nsp = io.of('/my-namespace');
-nsp.on('connection', function(socket){
-
-
     //displays when a user has connected and relays to connected clients
     console.log('a user connected');
-    io.of('/my-namespace').emit('user connected', 'A new user has joined the chat.');
+    io.emit('user connected', 'A new user has joined the chat.');
 
     //displays when a user has disconnected and relays to connected clients
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        io.of('/my-namespace').emit('user disconnected', 'A user has left the chat.');
+        io.emit('user disconnected', 'A user has left the chat.');
     });
 
     //displays chat messages serverside and relays to connected clients
     socket.on('chat message', (msg) => {
-        io.of('/my-namespace').emit('chat message', msg);
+        io.emit('chat message', msg);
         console.log('message: ' + msg);
     });
 
+    //custom namespace
+    const nsp = io.of('/my-namespace');
+    nsp.on('connection', function(socket){
+        console.log('someone connected');
+    });
 
-});    
-
-
-
-
+});
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
