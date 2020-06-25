c69f62d23cadf5278bedf985263cdc15e29bb662
Warren Brown <warrengbrn@gmail.com>
2020-06-25 14:21:22 -0400

    changing frontend to check for errors

--- public/scripts/frontendSocketIO.js
+++ public/scripts/frontendSocketIO.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    var socket = io('/my-namespace');
+    var socket = io('localhost:3000/my-namespace');
 
     $('#usernameCreateForm').submit(function(e){
         e.preventDefault();
