0fab142c96498a3d45bfed5e7a94db96507bd9af
Warren Brown <warrengbrn@gmail.com>
2020-07-18 12:18:20 -0400

    updating sql connection variables

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ var connection = ""
 if (process.env.NODE_ENV === "production"){
 
 var connection = mysql.createConnection({
-
+    host: process.env.SQL_HOST,
+    port: process.env.SQL_PORT,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE_NAME,
-    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
     multipleStatements: true
 });
 
